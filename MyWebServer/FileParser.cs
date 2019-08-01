3cb520956c986361e1d040da0bd9fc31b6ffebc3
Raunak singh <51930237+tavisca-rsingh@users.noreply.github.com>
2019-08-01 15:21:03 +0530

    Add files via upload

---
+++ MyWebServer/FileParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer
+{
+    public class FileParser
+    {
+       
+        static readonly string ServerPath = @"C:\Users\rsingh\source\repos\MyWebServer\MyWebServer\WebFile\";
+        public byte[] ParseFile(string rawUrl)
+        {
+            byte[] lines;
+
+            if (File.Exists(ServerPath + rawUrl))
+            {
+                lines = File.ReadAllBytes(ServerPath + rawUrl);
+            }
+            else
+            {
+                lines = File.ReadAllBytes(ServerPath + "notfound.jpg");
+            }
+         
+            return lines;
+           
+        }
+    }
+}
---
+++ MyWebServer/WebServer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer
+{
+    class WebServer
+    {
+        static void Main(string[] args)
+        {
+            string[] prefixes = { "http://localhost:8000/"};
+             Listener(prefixes);
+        }
+
+
+        public static void Listener(string[] prefixes)
+        {
+         
+            HttpListener listener = new HttpListener();
+            
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Console.WriteLine("Listening...");
+           
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();      
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+              
+                FileParser fileParser = new FileParser();
+                string rawUrl = request.RawUrl;
+                byte[] buffer = fileParser.ParseFile(rawUrl);
+
+                FileIdentifier fIleIdentifier = new FileIdentifier();
+               string fileType =  fIleIdentifier.IdentifyFileType(rawUrl);
+
+                response.AddHeader("Content-Type", fileType);
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+                
+            }
+
+           
+        }
+    }
+}
